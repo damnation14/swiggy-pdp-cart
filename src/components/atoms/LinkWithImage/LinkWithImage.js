3e667ff7e7b2de8da6ac1712c1d9df7c49772e08
damnation14 <manoj.kartik14@gmail.com>
2022-04-17 00:17:30 +0530

    minor chnages

--- src/components/atoms/LinkWithImage/LinkWithImage.js
+++ src/components/atoms/LinkWithImage/LinkWithImage.js
@@ -2,7 +2,7 @@ import {  React } from 'react';
 import styles from './logo.module.css'; 
 function LinkWithImage ({imgUrl,imgAlt,type}) {
    
-   
+        
         return (
                 <img className={type==="banner"?styles.banner_img:styles.logo_img} src={imgUrl} alt={imgAlt} ></img>
         );
